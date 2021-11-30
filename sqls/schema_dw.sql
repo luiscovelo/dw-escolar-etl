77539eced5283540b5fb5418d5cdf0521de084b7
Luis Covelo <luis.felipe@dropcode.com.br>
2021-11-30 17:52:30 -0300

    modified table name to plural

--- sqls/schema_dw.sql
+++ sqls/schema_dw.sql
@@ -1,41 +1,41 @@
-create table estado(
+create table estados(
 	id int not null auto_increment primary key,
 	nome varchar(255) not null
 );
 
-create table cidade(
+create table cidades(
 	id int not null auto_increment primary key,
 	nome varchar(255) not null,
 	estado_id int default 0 not null,
-	FOREIGN KEY (estado_id) REFERENCES estado(id)
+	FOREIGN KEY (estado_id) REFERENCES estados(id)
 );
 
-create table etnia(
+create table etnias(
 	id int not null auto_increment primary key,
 	nome varchar(255) not null
 );
 
-create table rede(
+create table redes(
 	id int not null auto_increment primary key,
 	nome varchar(255) not null
 );
 
-create table renda(
+create table rendas(
 	id int not null auto_increment primary key,
 	faixa varchar(255) not null
 );
 
-create table matricula(
+create table matriculas(
 	id int not null auto_increment primary key,
 	situacao varchar(255) not null
 );
 
-create table genero(
+create table generos(
 	id int not null auto_increment primary key,
 	nome varchar(2) not null
 );
 
-create table aluno(
+create table alunos(
 	id int not null auto_increment primary key,
 	cpf varchar(14) not null unique,
 	idade int default 0,
@@ -47,11 +47,11 @@ create table aluno(
 	rede_id int default 0 not null,
 	renda_id int default 0 not null,
 	matricula_id int default 0 not null,
-	FOREIGN KEY (estado_id) REFERENCES estado(id),
-	FOREIGN KEY (cidade_id) REFERENCES cidade(id),
-	FOREIGN KEY (etnia_id) REFERENCES etnia(id),
-	FOREIGN KEY (rede_id) REFERENCES rede(id),
-	FOREIGN KEY (renda_id) REFERENCES renda(id),
-	FOREIGN KEY (matricula_id) REFERENCES matricula(id),
-	FOREIGN KEY (genero_id) REFERENCES genero(id)
+	FOREIGN KEY (estado_id) REFERENCES estados(id),
+	FOREIGN KEY (cidade_id) REFERENCES cidades(id),
+	FOREIGN KEY (etnia_id) REFERENCES etnias(id),
+	FOREIGN KEY (rede_id) REFERENCES redes(id),
+	FOREIGN KEY (renda_id) REFERENCES rendas(id),
+	FOREIGN KEY (matricula_id) REFERENCES matriculas(id),
+	FOREIGN KEY (genero_id) REFERENCES generos(id)
 );
